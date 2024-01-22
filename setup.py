25c145f73e12f27d3c154457ce7afe13465b6e61
imensoltani <soltani.imen.enit@gmail.com>
2024-01-22 12:44:41 +0100

    the second commit setup

---
+++ setup.py
@@ -0,0 +1,28 @@
+## res to create/build our machine learning App as a package
+from setuptools import find_packages,setup
+from typing import List
+HYPHON_E_DOT ='-e .'
+def get_requirements(file_path:str)->List[str]:
+    '''
+    This function will return the list of requirements
+    '''
+    requirements =[]
+    with open('requirements.txt') as file_obj :
+        requirements=file_obj.readlines()
+        requirements=[req.replace("\n","") for req in requirements ]
+    if HYPHON_E_DOT in requirements:
+        requirements.remove(HYPHON_E_DOT)
+    return requirements
+
+setup(
+name='MLproject',
+version='0.0.1',
+author= 'Imen',
+author_email='soltani.imen.enit@gmail.com',
+packages=find_packages(),
+install_requires=get_requirements('requirements.txt')
+
+
+
+
+)
